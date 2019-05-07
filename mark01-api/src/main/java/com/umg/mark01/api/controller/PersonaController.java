ac2ffaebe51e77b2ed818b9da619dfd56b387b95
Manuel Mendez <manuel.mendez.ag@gmail.com>
2019-05-07 11:33:32 -0600

    update: webflux integration, emitterprocessor & reactive services

--- mark01-api/src/main/java/com/umg/mark01/api/controller/PersonaController.java
+++ mark01-api/src/main/java/com/umg/mark01/api/controller/PersonaController.java
@@ -6,10 +6,16 @@ import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Flux;
 
+import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 
 @Api(value = "/", description = "REST Persona")
@@ -20,6 +26,18 @@ public class PersonaController {
 
     private static List<Persona> listaPersonas = new ArrayList<>();
 
+
+    /* -------------------------------------------------------------------------------------------------------------- */
+    /**
+     * Notification emitter (emisor de notificaciones)
+     */
+    private EmitterProcessor<Persona> notificationProcessor;
+
+    /* -------------------------------------------------------------------------------------------------------------- */
+    @PostConstruct
+    private void createProcessor() {
+        notificationProcessor = EmitterProcessor.<Persona>create();
+    }
 
     /* -------------------------------------------------------------------------------------------------------------- */
     @RequestMapping(
@@ -37,6 +55,11 @@ public class PersonaController {
     public ResponseEntity<?> create(
             @RequestBody Persona entityParam) {
         listaPersonas.add(entityParam);
+
+        // cuando se crea una nueva persona.... notificar esta accion al emisor
+        System.out.println("Notificando nueva persona:" + entityParam.getPrimerNombre());
+        notificationProcessor.onNext(entityParam);
+
         return new ResponseEntity<>(entityParam, HttpStatus.OK);
     }
 
@@ -70,5 +93,68 @@ public class PersonaController {
     }
 
     /* -------------------------------------------------------------------------------------------------------------- */
+
+    /**
+     * Reactive functions (la respuesta hacia el cliente realmente incluye dos respuestas)
+     */
+    /* -------------------------------------------------------------------------------------------------------------- */
+
+    /**
+     * Flujo reactivo que contiene los datos de persona
+     *
+     * @return
+     */
+    private Flux<ServerSentEvent<Persona>> getPersonaSSE() {
+
+        // notification processor retorna un FLUX en el cual podemos estar "suscritos" cuando este tenga otro valor ...
+        return notificationProcessor
+                .log().map(
+                        (persona) -> {
+                            System.out.println("Sending Persona:" + persona.getId());
+                            return ServerSentEvent.<Persona>builder()
+                                    .id(UUID.randomUUID().toString())
+                                    .event("persona-result")
+                                    .data(persona)
+                                    .build();
+                        }).concatWith(Flux.never());
+    }
+
+    /* -------------------------------------------------------------------------------------------------------------- */
+
+    /**
+     * Flujo reactivo que posee un "heartbeat" para que la conexión del cliente se mantenga
+     *
+     * @return
+     */
+    private Flux<ServerSentEvent<Persona>> getNotificationHeartbeat() {
+        return Flux.interval(Duration.ofSeconds(15))
+                .map(i -> {
+                    System.out.println(String.format("sending heartbeat [%s] ...", i.toString()));
+                    return ServerSentEvent.<Persona>builder()
+                            .id(String.valueOf(i))
+                            .event("heartbeat-result")
+                            .data(null)
+                            .build();
+                });
+    }
+    /* -------------------------------------------------------------------------------------------------------------- */
+
+    /**
+     * Servicio reactivo que retorna la combinación de los dos flujos antes declarados
+     * Simplificacion de declaracion GET por "GetMapping"
+     *
+     * @return
+     */
+    /* -------------------------------------------------------------------------------------------------------------- */
+    @GetMapping(
+            value = "/notification/sse"
+    )
+    public Flux<ServerSentEvent<Persona>> getJobResultNotification() {
+
+        return Flux.merge(getNotificationHeartbeat(), getPersonaSSE());
+
+    }
+    /* -------------------------------------------------------------------------------------------------------------- */
+
 
 }
